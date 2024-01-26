6e0029a6eed4bd2cec946a989fc2f067a883dac1
yeoshuheng <yeoshuheng@gmail.com>
2024-01-26 15:38:13 +0800

    feat: added CIFAR10 loader

--- dataset/data.py
+++ dataset/data.py
@@ -1,6 +1,21 @@
-from torchvision import transforms
 from torch.utils.data import DataLoader
+from torchvision import datasets
+import torch, ssl
 from config import CONFIG
 
 BATCH_SIZE, IMG_SIZE = CONFIG["batch_size"], CONFIG["img_size"]
 
+def data_loader():
+    ssl._create_default_https_context = ssl._create_unverified_context
+    trainset = datasets.CIFAR10(root='./data', train=True,
+                                          download=True)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size = BATCH_SIZE,
+                                              shuffle=False, num_workers=2)
+    testset = datasets.CIFAR10(root='./data', train=False,
+                                         download=True)
+    testloader = torch.utils.data.DataLoader(testset, batch_size = 32,
+                                             shuffle=False, num_workers=2)
+    
+    testloader = torch.utils.data.DataLoader(testset, batch_size = 32,
+                                             shuffle=False, num_workers=2)
+    return trainloader, testloader
